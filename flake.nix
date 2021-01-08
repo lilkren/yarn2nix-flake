f710e09975187c7ef6051d513265bb008e5fc89a
lilkren <lilkren@gmail.com>
2021-01-08 23:04:27 +0000

    Sync

--- flake.nix
+++ flake.nix
@@ -13,12 +13,14 @@
         npkgs = ((import nixpkgs) {
             inherit system;
         });
-        yarn2Nix = (npkgs.callPackage ((import inputs.yarn2Nix)) {}).yarn2Nix;
+        # yarn2Nix = (npkgs.callPackage ((import inputs.yarn2Nix)) {}).yarn2Nix;
     in {
-        packages."${system}" = {
-            inherit yarn2Nix;
-        }; 
+        inherit inputs;
+        inherit npkgs;
+        # packages."${system}" = {
+        #     inherit yarn2Nix;
+        # }; 
 
-        defaultPackage."${system}" = self.packages.yarn2Nix;
+        # defaultPackage."${system}" = self.packages.yarn2Nix;
     };
 }
