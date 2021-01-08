c6e9a68ab42137dd471a6ee80e699d64a8356ab4
lilkren <lilkren@gmail.com>
2021-01-08 22:55:59 +0000

    Fix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
     inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
         yarn2Nix = {
             url = "github:Profpatsch/yarn2Nix";
             flake = false;
         };
     };
 
-  outputs = inputs@{ self, ... }: 
+  outputs = inputs@{ self, nixpkgs, ... }: 
     let 
       yarn2Nix = (nixpkgs.callPackage ((import inputs.yarn2Nix).yarn2Nix) {});
     in {
